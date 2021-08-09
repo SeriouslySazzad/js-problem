cf93d6d50b3d680b1e52f0c9a6402a0c73d2ce06
unknown <mahmudsazzad85@gmail.com>
2021-08-09 21:58:38 +0600

    commit

--- js-problem.js
+++ js-problem.js
@@ -1,44 +1,88 @@
-// 3. delevery cost
+// 1. Problem 01 (Seer to Mon Convert)
+function seerToMon(seer) {
+if (seer <= 0) {
+let error = "Enter a positive value"
+return error;
+}
+else {
+let mon = seer/40;
+return mon;
+};
+};
+const mon = seerToMon(800);
+console.log(mon);
+// ==============================================================
+// 2. Problem 02 (Total Sales)
+function totalSales(shirtQuantity, pantQuantity, shoesQuantity) {
+// Prices of Products
+const shirtPrice = 100;
+const pantPrice = 200;
+const shoesPrice = 500;
+// Price with Product Quantity
+const shirtTotal = shirtPrice * shirtQuantity;
+const pantTotal = pantPrice * pantQuantity;
+const shoesTotal = shoesPrice * shoesQuantity;
+// Total Price after the Sale of Shirt, Pant and Shoe
+const total = shirtTotal + pantTotal + shoesTotal;
+return total;
+}
+const totalOfSales = totalSales(5, 10, 5);
+if(totalOfSales <= 0){
+console.log("Input a positive value")
+}else{
+console.log(totalOfSales);
+};
+// ============================================================
+// 3. Problem 03 (Delivery Cost)
 function deliveryCost(tshirtNumber) {
-    const deliveryCostFirst100piece = 100;
-    const deliveryCostSecond200piece = 80;
-    const deliveryCostRestPiece = 50;
-    if(tshirtNumber <= 100) {
-        const first100DeliveryCost = tshirtNumber * deliveryCostFirst100piece;
-        return first100DeliveryCost;
-    }
-    else if(tshirtNumber > 100 && tshirtNumber <= 200) {
-        const first100deliveryCost = 100 * first100DeliveryCost;
-        const restTshirtNumber = tshirtNumber - 100;
-        const second200deliveryCost = restTshirtNumber * deliveryCostSecond200piece;
-        const totalDeliveryCost = first100deliveryCost + second200deliveryCost;
-        return totalDeliveryCost;
-    }
-    else{
-        const first100DeliveryCost = 100 * deliveryCostFirst100piece;
-        const second200deliveryCost = 80 * deliveryCostSecond200piece;
-        const restTshirtNumber = tshirtNumber - 200;
-        const restDeliveryCost = restTshirtNumber * deliveryCostRestPiece;
-        const totalDeliveryCost = first100DeliveryCost + second200deliveryCost + restDeliveryCost;
-        return totalDeliveryCost;
-    }
+// delivery costs criteria
+const deliveryCostFirst100piece = 100;
+const deliveryCostSecond200piece = 80;
+const deliveryCostRestPiece = 50;
+// condition for 1-100 tshirts
+if(tshirtNumber <= 100) {
+const first100DeliveryCost = tshirtNumber * deliveryCostFirst100piece;
+return first100DeliveryCost;
 }
-
-const finalDeliveryCost = deliveryCost(234);
+// condition for 101-200 tshirts
+else if(tshirtNumber <= 200) {
+const first100deliveryCost = 100 * deliveryCostFirst100piece;
+const restTshirtNumber = tshirtNumber - 100;
+const second200deliveryCost = restTshirtNumber * deliveryCostSecond200piece;
+const totalDeliveryCost = first100deliveryCost + second200deliveryCost;
+return totalDeliveryCost;
+}
+else if(tshirtNumber <= 0) {
+let error = "Enter a positive value"
+return error;
+}
+// condition for more than 200 tshirts
+else{
+const first100DeliveryCost = 100 * deliveryCostFirst100piece;
+const second200deliveryCost = 100 * deliveryCostSecond200piece;
+const restTshirtNumber = tshirtNumber - 200;
+const restDeliveryCost = restTshirtNumber * deliveryCostRestPiece;
+const totalDeliveryCost = first100DeliveryCost + second200deliveryCost + restDeliveryCost;
+return totalDeliveryCost;
+}
+}
+const finalDeliveryCost = deliveryCost(225);
 console.log(finalDeliveryCost);
-
-// 4. Perfect Friend
-// 4. Perfect Friend
+// =============================================================
+// 4. Problem 04 (Perfect Friend)
 function perfectFriend(friends) {
-    let element = friends[0];
-    for(let i = 0; i < friends.length; i++) {
-        let element = friends[i];
-        if(element.length == 5) {
-            return element;
-        }
-    }
+for(let i = 0; i < friends.length; i++) {
+let element = friends[i];
+if(element.length == 5) {
+return element;
 }
-
-const friends = ['lipi', 'mahfuz', 'mukit', 'foysal', 'tamim', 'tanzina'];
-var bigNameFriend = perfectFriend(friends);
-console.log(bigNameFriend);
+else if(typeof friends[i] == 'number') {
+let error = 'Please enter strings in the array';
+return error;
+}
+}
+}
+const friends = ['Shakib', 'Mushfiq', 'Mustafiz', 'Tamim', 'Mahmudullah','akiba'];
+var perfectFriendName = perfectFriend(friends);
+console.log(perfectFriendName);
+// ===========================================================
